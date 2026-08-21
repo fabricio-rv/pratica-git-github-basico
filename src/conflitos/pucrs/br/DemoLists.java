5c606f3f0cf9107c5c7365139321e9e710cc7e5b
Fabricio Rassier <fabriciorassier@gmail.com>
2026-08-21 20:36:49 -0300

    adiciona metodos hasRepeat e union

--- src/conflitos/pucrs/br/DemoLists.java
+++ src/conflitos/pucrs/br/DemoLists.java
@@ -24,4 +24,25 @@ public class DemoLists {
         return repetidos;
     }
 
+    boolean hasRepeat(ArrayList<Integer> l) {
+        for (int i = 0; i < l.size(); i++) {
+            for (int j = i + 1; j < l.size(); j++) {
+                if (l.get(i).equals(l.get(j))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    ArrayList<Integer> union(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+        ArrayList<Integer> resultado = new ArrayList<>(l1);
+        for (Integer el : l2) {
+            if (!resultado.contains(el)) {
+                resultado.add(el);
+            }
+        }
+        return resultado;
+    }
+
 }
