972a75ef2203535cd367762c90b6fe78f2341987
Fabricio Rassier <fabriciorassier@gmail.com>
2026-08-21 20:38:08 -0300

    adiciona demonstracao dos metodos no App

--- src/conflitos/pucrs/br/App.java
+++ src/conflitos/pucrs/br/App.java
@@ -1,7 +1,27 @@
 package conflitos.pucrs.br;
 
+import java.util.ArrayList;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        DemoLists demo = new DemoLists();
+
+        ArrayList<Integer> l = new ArrayList<>();
+        l.add(1); l.add(2); l.add(3); l.add(2); l.add(4); l.add(1);
+
+        System.out.println("Lista: " + l);
+        System.out.println("Ocorrencias do 2: " + demo.nOcorrencias(l, 2));
+        System.out.println("Tem repetidos: " + demo.hasRepeat(l));
+        System.out.println("Numero de repetidos: " + demo.nroRepeat(l));
+        System.out.println("Lista de repetidos: " + demo.listRepeat(l));
+
+        ArrayList<Integer> l1 = new ArrayList<>();
+        l1.add(1); l1.add(2); l1.add(3);
+
+        ArrayList<Integer> l2 = new ArrayList<>();
+        l2.add(2); l2.add(3); l2.add(4);
+
+        System.out.println("Uniao: " + demo.union(l1, l2));
+        System.out.println("Interseccao: " + demo.intersect(l1, l2));
     }
 }
