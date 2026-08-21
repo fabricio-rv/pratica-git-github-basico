5823f5e54212d503c7369413115f4883b0ec57d1
Fabricio Rassier <fabriciorassier@gmail.com>
2026-08-21 20:34:29 -0300

    adiciona metodos nOcorrencias e listRepeat

--- src/conflitos/pucrs/br/DemoLists.java
+++ src/conflitos/pucrs/br/DemoLists.java
@@ -1,5 +1,27 @@
 package conflitos.pucrs.br;
 
+import java.util.ArrayList;
+
 public class DemoLists {
-    
+
+    int nOcorrencias(ArrayList<Integer> l, Integer el) {
+        int count = 0;
+        for (Integer i : l) {
+            if (i.equals(el)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    ArrayList<Integer> listRepeat(ArrayList<Integer> l) {
+        ArrayList<Integer> repetidos = new ArrayList<>();
+        for (Integer el : l) {
+            if (nOcorrencias(l, el) > 1 && !repetidos.contains(el)) {
+                repetidos.add(el);
+            }
+        }
+        return repetidos;
+    }
+
 }
